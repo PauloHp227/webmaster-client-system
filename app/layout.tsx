07cda9ca7ed1cbcdb2c01261561965a26ba9c411
Paulo Henrique <phenriquehp227@gmail.com>
2026-06-04 11:20:08 -0300

    Configurado logo para compartilhamento

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,12 +2,40 @@ import "./globals.css";
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Webmaster Digital",
-  description: "Sistema de Gestão Webmaster Digital",
+  title: "Webmaster Digital | Sistema de Clientes",
+  description:
+    "Sistema para clientes, projetos, contratos e briefings da Webmaster Digital",
+
   icons: {
     icon: "/favicon.ico",
     shortcut: "/favicon.ico",
     apple: "/favicon.ico",
+  },
+
+  openGraph: {
+    title: "Webmaster Digital | Sistema de Clientes",
+    description:
+      "Sistema para clientes, projetos, contratos e briefings da Webmaster Digital",
+    url: "https://webmaster-client-system.vercel.app",
+    siteName: "Webmaster Digital",
+    images: [
+      {
+        url: "/logo.png",
+        width: 1200,
+        height: 630,
+        alt: "Webmaster Digital",
+      },
+    ],
+    locale: "pt_BR",
+    type: "website",
+  },
+
+  twitter: {
+    card: "summary_large_image",
+    title: "Webmaster Digital | Sistema de Clientes",
+    description:
+      "Sistema para clientes, projetos, contratos e briefings da Webmaster Digital",
+    images: ["/logo.png"],
   },
 };
 
